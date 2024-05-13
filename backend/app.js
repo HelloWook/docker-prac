b4538222a9bf2107786dbe8ae3bdf84a05bfc287
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-05-14 00:49:10 +0900

    chore:nginx 설정

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,8 @@
 const express = require("express");
-const cors = require("cors");
 const bodyParser = require("body-parser");
 const path = require("path");
 require("dotenv").config({ path: path.join(__dirname, "/config/.env") });
 const app = express();
-
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
